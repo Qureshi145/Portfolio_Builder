443f637ce406fe5a4e975adf317f525177edbc64
Abd-2001 <157145697+Abd-2001@users.noreply.github.com>
2024-03-25 14:44:57 +0500

    Signup.jsx
    Signup.jsx (Update)

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -5,44 +5,44 @@ import { Link } from "react-router-dom";
 const Signup = () => {
   return (
     <>
-    <section>
-      <form>
-    <div>
-      <div className="containerls">
-        <div style={{ background: "#60cfa5" }} className="header1">
-          <div style={{ background: "#60cfa5", color:"black" }} className="text">Sign Up</div>
-        </div>
-        
-        <div style={{ background: "#60cfa5" }}className="textboxes">
-          <div className="input">
-            <input type="text" placeholder="First Name" />
-          </div>
-          <div className="input">
-            <input type="text" placeholder="Last Name" />
-          </div>
-          <div className="input">
-            <input type="email" placeholder="Email" />
-          </div>
-          <div className="input">
-            <input type="password" placeholder="Password" />
-          </div>
-          
-        </div>
-        <span style={{ background: "#60cfa5" }} className="check">
-          <input type="checkbox" /> By signing up, I agree with the Terms of Use
-          and Privacy Policy
-        </span>
-        <div className="submit-container">
+      <section>
+        <form>
           <div>
-            <button className="submit">Sign Up</button>
+            <div className="containerls">
+              <div className="header1">
+                <div className="text" style={{ backgroundColor: "#60cfa5", color:"black" }}>Sign Up</div>
+              </div>
+
+              <div className="textboxes" style={{ backgroundColor: "#60cfa5" }}>
+                <div className="input" style={{ width: "400px", height: "40px" }}>
+                  <input type="text" placeholder="First Name" />
+                </div>
+                <div className="input" style={{ width: "400px", height: "40px" }}>
+                  <input type="text" placeholder="Last Name" />
+                </div>
+                <div className="input" style={{ width: "400px", height: "40px" }}>
+                  <input type="email" placeholder="Email" />
+                </div>
+                <div className="input" style={{ width: "400px", height: "40px" }}>
+                  <input type="password" placeholder="Password" />
+                </div>
+
+              </div>
+              <span className="check" style={{ backgroundColor: "#60cfa5" }}>
+                <input type="checkbox" /> By signing up, I agree with the Terms of Use
+                and Privacy Policy
+              </span>
+              <div className="submit-container">
+                <div>
+                  <button className="submit">Sign Up</button>
+                </div>
+              </div>
+              <span style={{ backgroundColor: "#60cfa5" }} className="login-signup">
+                Already have an account? <Link to="/login" style={{ backgroundColor: "#60cfa5" }}>Login</Link>
+              </span>
+            </div>
           </div>
-        </div>
-        <span style={{ background: "#60cfa5" }} className="login-signup">
-          Already have an account? <Link to="/login" style={{ background: "#60cfa5" }}>Login</Link>
-        </span>
-      </div>
-      </div>
-      </form>
+        </form>
       </section>
     </>
   );
