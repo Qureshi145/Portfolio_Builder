d8a4497bf1ea03da0ae39166bf116fb14ae41e8e
Abd-2001 <157145697+Abd-2001@users.noreply.github.com>
2024-03-25 14:44:14 +0500

    Login.jsx
    Login.jsx (Updated)

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -5,35 +5,35 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <>
-    <section>
-      <form>
-        <div className="bg">
-      <div className="containerls">
-        <div style={{ background: "#60cfa5" }} className="header1">
-          <div  style={{ background: "#60cfa5", color:"black" }} className="text">Login</div>
-        </div>
-        <div style={{ background: "#60cfa5" }} className="textboxes">
-          <div className="input">
-            <input type="email" placeholder="Email" />
+      <section>
+        <form>
+          <div className="bg">
+            <div className="containerls">
+              <div className="header1">
+                <div className="text" style={{ backgroundColor: "#60cfa5", color:"black" }}>Login</div>
+              </div>
+              <div className="textboxes" style={{ backgroundColor: "#60cfa5" }}>
+                <div className="input" style={{ width: "400px", height: "40px" }}>
+                  <input type="email" placeholder="Email" />
+                </div>
+                <div className="input" style={{ width: "400px", height: "40px" }}>
+                  <input type="password" placeholder="Password" />
+                </div>
+              </div>
+              <span className="check" style={{ backgroundColor: "#60cfa5" }}>
+                <input type="checkbox" /> Remember me on this device
+              </span>
+              <div className="submit-container">
+                <div>
+                  <button className="submit">Login</button>
+                </div>
+              </div>
+              <span className="login-signup" style={{ backgroundColor: "#60cfa5" }}>
+                Don't have an account? <Link to="/signup" style={{ backgroundColor: "#60cfa5" }}>Sign Up</Link>
+              </span>
+            </div>
           </div>
-          <div className="input">
-            <input type="password" placeholder="Password" />
-          </div>
-        </div>
-        <span style={{ background: "#60cfa5" }} className="check">
-          <input type="checkbox" /> Remember me on this device
-        </span>
-        <div className="submit-container">
-          <div>
-            <button className="submit">Login</button>
-          </div>
-        </div>
-        <span style={{ background: "#60cfa5" }} className="login-signup">
-          Don't have an account? <Link to="/signup" style={{ background: "#60cfa5" }}>Sign Up</Link>
-        </span>
-      </div>
-      </div>
-      </form>
+        </form>
       </section>
     </>
   );
